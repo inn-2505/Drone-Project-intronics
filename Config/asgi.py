a9ce60d4fe92e91c333519c5e455b21c7c4cd60d
Gene <phattiran.k@gmail.com>
2026-06-29 16:11:34 +0700

    web socket and action button

--- Config/asgi.py
+++ Config/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from myapp.consumers import DroneConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/drone/", DroneConsumer.as_asgi()),
+        ])
+    ),
+})
---
+++ myapp/consumers.py
@@ -0,0 +1,32 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class DroneConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = "drone_f722_control"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    # receive data from dashboard
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        command = data.get('command')
+        
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'drone_command_message',
+                'command': command
+            }
+        )
+
+    # send data to ESP32
+    async def drone_command_message(self, event):
+        command = event['command']
+        await self.send(text_data=json.dumps({
+            'command': command,
+            'type': 'LIVE_COMMAND'
+        }))
